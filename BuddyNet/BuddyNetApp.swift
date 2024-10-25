9b7f7d4bae7308fbbe0b90e88a3c385ce0c91503
Sergey Eliseev <sergei.eliseev17@icloud.com>
2024-10-25 12:44:26 +0300

    Initial Commit

---
+++ BuddyNet/BuddyNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuddyNetApp.swift
+//  BuddyNet
+//
+//  Created by Serge Eliseev on 25.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct BuddyNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
